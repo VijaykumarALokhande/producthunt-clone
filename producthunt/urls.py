88e0cfbd5c388b0f9ff9c63dc0d34413bad622d2
Vijaykumar Arun Lokhande <vijaysaurabh1999@gmail.com>
2021-03-26 20:45:30 +0530

    defined homepage from products apps views file.

--- producthunt/urls.py
+++ producthunt/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from . import views
-
+# from . import views
+import products.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.homepage, name = 'firsthomepage'),
+    path('', products.views.home, name = 'firsthomepage'),
 ]
