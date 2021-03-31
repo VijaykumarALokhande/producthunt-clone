efde55880f24cdc88a07c7fac9bcf2c64f6c811d
Vijaykumar Arun Lokhande <vijaysaurabh1999@gmail.com>
2021-03-31 20:25:36 +0530

    files rearranged and logo is added

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -44,7 +44,7 @@ ROOT_URLCONF = 'producthunt.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ['producthunt/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
