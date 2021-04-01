5bf00efd203c922370c690a3aba1aeea5a8f70ea
Vijaykumar Arun Lokhande <vijaysaurabh1999@gmail.com>
2021-04-01 22:37:59 +0530

    now a product can be created at the site.

--- producthunt/urls.py
+++ producthunt/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 # from . import views
 import products.views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', products.views.home, name = 'firsthomepage'),
     path('accounts/', include('accounts.urls') ),
     path('products/', include('products.urls') ),
-]
+]+static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- products/views.py
+++ products/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .models import ProductMC
+from django.utils import timezone
 def home(request):
     return render(request, 'products/home.html')
 
 @login_required
 def createfn(request):
-    return render(request, 'products/create.html')
+    if request.method=="POST":
+        if request.POST['title'] and request.POST['body'] and request.POST['product_url'] and request.FILES['icon'] and request.FILES['product_image']:
+            obj = ProductMC()
+            obj.title = request.POST['title']
+            obj.body = request.POST['body']
+            if request.POST['product_url'].startswith("https://") or request.POST['product_url'].startswith("http://"):
+                obj.url = request.POST['product_url']
+            else:
+                obj.url = "https://" +request.POST['product_url']
+            obj.icon = request.FILES['icon']
+            obj.image = request.FILES['product_image']
+            obj.pub_date = timezone.datetime.now()
+            obj.hunter = request.user
+            obj.save()
+            return redirect('firsthomepage')
+        else:
+            return render(request, 'products/create.html', {'error':'enter all product details'})
+    else:
+        return render(request, 'products/create.html')
